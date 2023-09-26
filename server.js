670782a35b3227a6b1cab29208d2cefb7318e9d8
lmtgle104 <letunglam904@gmail.com>
2023-09-26 16:55:38 -0400

    initial  commit

---
+++ server.js
@@ -0,0 +1,69 @@
+/*********************************************************************************
+*  BTI325 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: ______________________ Student ID: ______________ Date: ________________
+*
+*  Online (Cyclic) Link: ________________________________________________________
+*
+********************************************************************************/ 
+
+const express = require('express'); // "require" the Express module
+const app = express(); // obtain the "app" object
+const HTTP_PORT = process.env.PORT || 8080; // assign a port
+
+const path = require("path");
+const blogService = require("./blog-service.js");
+app.get('/', (reg, res) => {
+    res.redirect('/about');
+});
+app.get("/about", (req, res) =>{
+    res.sendFile(__dirname + "/views/about.html");
+});
+app.use(express.static('public'));
+
+// start the server on the port and output a confirmation ot the console
+app.listen(HTTP_PORT, () => console.log(`Express http server listening on: ${HTTP_PORT}`));
+
+app.get("/blog", (req, res) => {
+    blogService 
+      .getPublishedPosts()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch(function (err) {
+        console.log("Unable to open the file: " + err);
+      });
+  });
+  
+  app.get("/posts", (req, res) => {
+    blogService
+      .getAllPosts()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch(function (err) {
+        console.log("Unable to open the file: " + err);
+      });
+  });
+  
+  app.get("/categories", (req, res) => {
+    blogService
+      .getCategories()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch(function (err) {
+        console.log("Unable to open the file: " + err);
+      });
+  });
+  
+  app.get("*", (req, res) => {
+    res.status(404).sendFile(__dirname + "/views/404/error.html");
+  });
+
+
+  
+
+ 
