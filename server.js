28213598feee6abc813403384b0ff2cb46379703
lmtgle104 <142159547+lmtgle104@users.noreply.github.com>
2023-09-28 17:47:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,54 +12,50 @@
 const express = require('express'); // "require" the Express module
 const app = express(); // obtain the "app" object
 const HTTP_PORT = process.env.PORT || 8080; // assign a port
-
-const path = require("path");
 const blogService = require("./blog-service.js");
-app.get('/', (reg, res) => {
+app.get('/', (req, res) => {
     res.redirect('/about');
-});
-app.get("/about", (req, res) =>{
+  });
+  app.get("/about", (req, res) => {
     res.sendFile(__dirname + "/views/about.html");
-});
+  });
 app.use(express.static('public'));
-
-
 app.get("/blog", (req, res) => {
-    blogService 
-      .getPublishedPosts()
-      .then((data) => {
-        res.json(data);
-      })
-      .catch(function (err) {
-        console.log("Unable to open the file: " + err);
-      });
-  });
-  
-  app.get("/posts", (req, res) => {
-    blogService
-      .getAllPosts()
-      .then((data) => {
-        res.json(data);
-      })
-      .catch(function (err) {
-        console.log("Unable to open the file: " + err);
-      });
-  });
+  blogService
+    .getPublishedPosts()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(function (err) {
+      console.log("Unable to open the file: " + err);
+    });
+});
 
- app.get("/categories", (req, res) => {
-    blogService
-      .getCategories()
-      .then((data) => {
-        res.json(data);
-      })
-      .catch(function (err) {
-        console.log("Unable to open the file: " + err);
-      });
-  });
-  
-  app.get("*", (req, res) => {
-    res.status(404).sendFile(__dirname + "/views/404/error.html");
-  });
+app.get("/posts", (req, res) => {
+  blogService
+    .getAllPosts()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(function (err) {
+      console.log("Unable to open the file: " + err);
+    });
+});
+
+app.get("/categories", (req, res) => {
+  blogService
+    .getCategories()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(function (err) {
+      console.log("Unable to open the file: " + err);
+    });
+});
+
+app.get("*", (req, res) => {
+  res.status(404).sendFile(__dirname + "/views/404/error.html");
+});
 
 blogService
 .initialize()
@@ -68,9 +64,8 @@ blogService
   })
   .catch(function(err){
     console.log("Unable to open file: "+ err);
-  });
+  })
 
 
-  // start the server on the port and output a confirmation ot the console
-app.listen(HTTP_PORT, () => console.log(`Express http server listening on: ${HTTP_PORT}`));
+
  
